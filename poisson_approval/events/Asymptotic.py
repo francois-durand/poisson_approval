42a20a831cb7f7dd86ef21bd8a72fa1e15f1d0c7
francois-durand <fradurandpub@gmail.com>
2020-01-16 14:23:43 +0100

    Update Asymptotic.py
    Do not count method _repr_pretty_ in Codecov.

--- poisson_approval/events/Asymptotic.py
+++ poisson_approval/events/Asymptotic.py
@@ -110,7 +110,7 @@ class Asymptotic:
         s = "exp(%s)" % s
         return s
 
-    def _repr_pretty_(self, p, cycle):
+    def _repr_pretty_(self, p, cycle):  # pragma : no cover
         # https://stackoverflow.com/questions/41453624/tell-ipython-to-use-an-objects-str-instead-of-repr-for-output
         p.text(str(self) if not cycle else '...')
 
