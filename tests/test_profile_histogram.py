e872ce41474bc12b8723d19df9e71ff1e4c19919
francois-durand <fradurandpub@gmail.com>
2020-01-18 06:33:32 +0100

    Add test

--- tests/test_profile_histogram.py
+++ tests/test_profile_histogram.py
@@ -1,6 +1,6 @@
 import numpy as np
 from fractions import Fraction
-from poisson_approval import ProfileHistogram
+from poisson_approval import ProfileHistogram, StrategyThreshold, EquilibriumStatus
 
 
 def test():
@@ -9,3 +9,9 @@ def test():
                                d_ranking_histogram = {'abc': [1, 1], 'acb': [1]})
     assert profile.abc == 0.5
     assert np.all(profile.d_ranking_histogram['abc'] == [0.5, 0.5])
+
+    # Check a strategy that is not an equilibrium
+    profile = ProfileHistogram(d_ranking_share={'abc': 0.5, 'acb': 0.5},
+                               d_ranking_histogram = {'abc': [1], 'acb': [1]})
+    strategy = StrategyThreshold(d_ranking_threshold={'abc': 0, 'acb': 0})
+    assert profile.is_equilibrium(strategy) == EquilibriumStatus.NOT_EQUILIBRIUM
