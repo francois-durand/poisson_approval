a5308ec0981a613ef7eb8cddcd10783ee9fb5ff0
francois-durand <fradurandpub@gmail.com>
2020-02-03 16:20:40 +0100

    Refactor BestResponse
    Refactoring without any behavior modification. Purpose:
    * Make the code more modular and readable.
    * Enable to investigate the compared precisions of the two methods when both are able to compute the threshold utility.

--- poisson_approval/best_response/BestResponse.py
+++ poisson_approval/best_response/BestResponse.py
@@ -1,6 +1,7 @@
 from math import isclose
-from poisson_approval.utils.Util import isnan, ballot_one, ballot_one_two
 from poisson_approval.constants.constants import *
+from poisson_approval.utils.Util import isnan, ballot_one, ballot_one_two
+from poisson_approval.utils.UtilCache import cached_property
 
 
 class BestResponse:
@@ -20,20 +21,6 @@ class BestResponse:
         Second `personalized trio`.
     trio : EventTrio
         3-candidate tie.
-
-    Attributes
-    ----------
-    ballot : str
-        This can be a valid ballot (e.g. ``'a'`` or ``'ab'`` if `ranking` is ``'abc'``) or ``'utility-dependent'``.
-        Historically, it could also be ``'inconclusive'``, but this value is not used anymore in the current version
-        of the algorithm.
-    threshold_utility : Number
-        The threshold value of the utility for the second candidate (where the optimal ballot changes). If 1, then
-        always vote for the first candidate. If 0, then always vote for the two most-liked candidates.
-    justification : str
-        How the program made its decision. Nowadays, possible values are ``'Asymptotic method'``,
-        ``'Simplified asymptotic method'``, ``'Easy vs difficult pivot'``, ``'Difficult vs easy pivot'``,
-        ``'Offset method'``.
 
     Examples
     --------
@@ -58,80 +45,132 @@ class BestResponse:
     OFFSET_METHOD = 'Offset method'
 
     def __init__(self, ranking, pivot_tij, pivot_tjk, trio_1t, trio_2t, trio):
-        # -------------
-        # Preliminaries
-        # -------------
         self.ranking = ranking
         self.pivot_tij = pivot_tij
         self.pivot_tjk = pivot_tjk
         self.trio_1t = trio_1t
         self.trio_2t = trio_2t
         self.trio = trio
-        # -----------
-        # Actual work
-        # -----------
-        self.threshold_utility = ((
-            pivot_tij.asymptotic * (1 / 2)
-            + trio_1t.asymptotic * (1 / 3) + trio_2t.asymptotic * (1 / 6)
+
+    @cached_property
+    def results_asymptotic_method(self):
+        """tuple (threshold_utility, justification)
+
+        Results according to the asymptotic method. Cf. :attr:`threshold_utility` and :attr:`justification`.
+        The threshold utility may be NaN, because the asymptotic method is not always sufficient.
+        """
+        threshold_utility = ((
+            self.pivot_tij.asymptotic * (1 / 2)
+            + self.trio_1t.asymptotic * (1 / 3) + self.trio_2t.asymptotic * (1 / 6)
         ) / (
-            pivot_tij.asymptotic * (1 / 2) + pivot_tjk.asymptotic * (1 / 2)
-            + trio_1t.asymptotic * (2 / 3) + trio_2t.asymptotic * (1 / 3)
+            self.pivot_tij.asymptotic * (1 / 2) + self.pivot_tjk.asymptotic * (1 / 2)
+            + self.trio_1t.asymptotic * (2 / 3) + self.trio_2t.asymptotic * (1 / 3)
         )).limit
-        self.justification = self.ASYMPTOTIC
+        justification = self.ASYMPTOTIC
+        return threshold_utility, justification
+
+    @cached_property
+    def results_limit_pivot_theorem(self):
+        """tuple (threshold_utility, justification)
+
+        Results according to the limit pivot theorem. Cf. :attr:`threshold_utility` and :attr:`justification`.
+        """
+        phi_i = getattr(self.trio, 'phi_' + self.ranking[0])
+        if isnan(phi_i):
+            phi_i = (getattr(self.trio, 'phi_' + self.ranking[0] + self.ranking[1])
+                     * getattr(self.trio, 'phi_' + self.ranking[0] + self.ranking[2]))
+        phi_j = getattr(self.trio, 'phi_' + self.ranking[1])
+        if isnan(phi_j):
+            phi_j = (getattr(self.trio, 'phi_' + self.ranking[1] + self.ranking[0])
+                     * getattr(self.trio, 'phi_' + self.ranking[1] + self.ranking[2]))
+        phi_k = getattr(self.trio, 'phi_' + self.ranking[2])
+        if isnan(phi_k):
+            phi_k = (getattr(self.trio, 'phi_' + self.ranking[2] + self.ranking[0])
+                     * getattr(self.trio, 'phi_' + self.ranking[2] + self.ranking[1]))
+        phi_ij = getattr(self.trio, 'phi_' + self.ranking[0] + self.ranking[1])
+        if isnan(phi_ij):
+            phi_ij = getattr(self.trio, 'phi_' + self.ranking[0]) * getattr(self.trio, 'phi_' + self.ranking[1])
+        phi_ik = getattr(self.trio, 'phi_' + self.ranking[0] + self.ranking[2])
+        if isnan(phi_ik):
+            phi_ik = getattr(self.trio, 'phi_' + self.ranking[0]) * getattr(self.trio, 'phi_' + self.ranking[2])
+        phi_i_ge_1 = (isclose(phi_i, 1) or phi_i >= 1)
+        phi_k_ge_1 = (isclose(phi_k, 1) or phi_k >= 1)
+        if phi_i_ge_1 and phi_k_ge_1:
+            # Both pivots are easy: we can forget the trios
+            threshold_utility = ((
+                self.pivot_tij.asymptotic * (1 / 2)
+            ) / (
+                self.pivot_tij.asymptotic * (1 / 2) + self.pivot_tjk.asymptotic * (1 / 2)
+            )).limit
+            justification = self.ASYMPTOTIC_SIMPLIFIED
+        elif phi_i_ge_1:
+            # Pivot jk is difficult, pivot ij is easy
+            threshold_utility = 0
+            justification = self.EASY_VS_DIFFICULT
+        elif phi_k_ge_1:
+            # Pivot ij is difficult, pivot jk is easy
+            threshold_utility = 1
+            justification = self.DIFFICULT_VS_EASY
+        else:
+            # Both pivots are difficult -> General case of the offset method
+            pij = (1 + phi_ik) / (1 - phi_k)
+            pjk = (1 + phi_j) * phi_i ** 2 / (1 - phi_i)
+            p1t = phi_i
+            p2t = phi_ij
+            threshold_utility = (pij / 2 + p1t / 3 + p2t / 6) / (pij / 2 + pjk / 2 + p1t * 2 / 3 + p2t / 3)
+            justification = self.OFFSET_METHOD
+        return threshold_utility, justification
+
+    @cached_property
+    def results(self):
+        """tuple (threshold_utility, justification)
+
+        Results that use the asymptotic method if possible (better numerical approximation, but does not always provide
+        the threshold utility), and the limit pivot theorem otherwise (that always computes a threshold utility). Cf.
+        :attr:`threshold_utility` and :attr:`justification`.
+        """
+        threshold_utility, justification = self.results_asymptotic_method
+        if isnan(threshold_utility):
+            threshold_utility, justification = self.results_limit_pivot_theorem
+        return threshold_utility, justification
+
+    @cached_property
+    def threshold_utility(self):
+        """Number
+
+        The threshold value of the utility for the second candidate (where the optimal ballot changes). If 1, then
+        always vote for the first candidate. If 0, then always vote for the two most-liked candidates.
+        """
+        return self.results[0]
+
+    @cached_property
+    def justification(self):
+        """str
+
+        How the program computed the utility threshold. Nowadays, possible values are ``'Asymptotic method'``,
+        ``'Simplified asymptotic method'``, ``'Easy vs difficult pivot'``, ``'Difficult vs easy pivot'``,
+        ``'Offset method'``.
+        """
+        return self.results[1]
+
+    @cached_property
+    def ballot(self):
+        """str
+
+        This can be a valid ballot (e.g. ``'a'`` or ``'ab'`` if `ranking` is ``'abc'``) or ``'utility-dependent'``.
+        Historically, it could also be ``'inconclusive'``, but this value is not used anymore in the current version
+        of the algorithm.
+        """
         if isnan(self.threshold_utility):
-            phi_i = getattr(trio, 'phi_' + ranking[0])
-            if isnan(phi_i):
-                phi_i = (getattr(trio, 'phi_' + ranking[0] + ranking[1])
-                         * getattr(trio, 'phi_' + ranking[0] + ranking[2]))
-            phi_j = getattr(trio, 'phi_' + ranking[1])
-            if isnan(phi_j):
-                phi_j = (getattr(trio, 'phi_' + ranking[1] + ranking[0])
-                         * getattr(trio, 'phi_' + ranking[1] + ranking[2]))
-            phi_k = getattr(trio, 'phi_' + ranking[2])
-            if isnan(phi_k):
-                phi_k = (getattr(trio, 'phi_' + ranking[2] + ranking[0])
-                         * getattr(trio, 'phi_' + ranking[2] + ranking[1]))
-            phi_ij = getattr(trio, 'phi_' + ranking[0] + ranking[1])
-            if isnan(phi_ij):
-                phi_ij = getattr(trio, 'phi_' + ranking[0]) * getattr(trio, 'phi_' + ranking[1])
-            phi_ik = getattr(trio, 'phi_' + ranking[0] + ranking[2])
-            if isnan(phi_ik):
-                phi_ik = getattr(trio, 'phi_' + ranking[0]) * getattr(trio, 'phi_' + ranking[2])
-            phi_i_ge_1 = (isclose(phi_i, 1) or phi_i >= 1)
-            phi_k_ge_1 = (isclose(phi_k, 1) or phi_k >= 1)
-            if phi_i_ge_1 and phi_k_ge_1:
-                # Both pivots are easy: we can forget the trios
-                self.threshold_utility = ((
-                    pivot_tij.asymptotic * (1 / 2)
-                ) / (
-                    pivot_tij.asymptotic * (1 / 2) + pivot_tjk.asymptotic * (1 / 2)
-                )).limit
-                self.justification = self.ASYMPTOTIC_SIMPLIFIED
-            elif phi_i_ge_1:
-                # Pivot jk is difficult, pivot ij is easy
-                self.threshold_utility = 0
-                self.justification = self.EASY_VS_DIFFICULT
-            elif phi_k_ge_1:
-                # Pivot ij is difficult, pivot jk is easy
-                self.threshold_utility = 1
-                self.justification = self.DIFFICULT_VS_EASY
-            else:
-                # Both pivots are difficult -> General case of the offset method
-                pij = (1 + phi_ik) / (1 - phi_k)
-                pjk = (1 + phi_j) * phi_i**2 / (1 - phi_i)
-                p1t = phi_i
-                p2t = phi_ij
-                self.threshold_utility = (pij / 2 + p1t / 3 + p2t / 6) / (pij / 2 + pjk / 2 + p1t * 2 / 3 + p2t / 3)
-                self.justification = self.OFFSET_METHOD
-        if isnan(self.threshold_utility):
-            self.ballot = INCONCLUSIVE  # pragma: no cover
+            ballot = INCONCLUSIVE  # pragma: no cover
             raise ValueError('Unable to compute threshold utility')  # pragma: no cover
         elif isclose(self.threshold_utility, 1):
-            self.ballot = ballot_one(ranking)
+            ballot = ballot_one(self.ranking)
         elif isclose(self.threshold_utility, 0):
-            self.ballot = ballot_one_two(ranking)
+            ballot = ballot_one_two(self.ranking)
         else:
-            self.ballot = UTILITY_DEPENDENT
+            ballot = UTILITY_DEPENDENT
+        return ballot
 
     def __repr__(self):
         return '<' + ', '.join([
