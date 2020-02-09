1d12dbe546aaafc3f4216da153ef92ad5a5b62bb
francois-durand <fradurandpub@gmail.com>
2020-02-09 07:45:56 +0100

    Bump version: 0.8.1 → 0.9.0

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/francois-durand/poisson_approval',
-    version='0.8.1',
+    version='0.9.0',
     zip_safe=False,
 )
