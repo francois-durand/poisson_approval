d515aede56db6c428286a1aba62dc99b73628bc3
francois-durand <fradurandpub@gmail.com>
2020-02-09 06:54:23 +0100

    Improve coverage

--- tests/test_profile_discrete.py
+++ tests/test_profile_discrete.py
@@ -1,6 +1,28 @@
-from poisson_approval import ProfileDiscrete
+from poisson_approval import ProfileDiscrete, PLURALITY, ANTI_PLURALITY
 
 
 def test_normalization():
     profile = ProfileDiscrete({('abc', 0.2): 1, ('acb', 0.4): 1})
     assert profile.abc == 0.5
+
+
+def test_plurality():
+    """
+        >>> profile = ProfileDiscrete({('abc', 0.2): 1, ('acb', 0.4): 1}, voting_rule=PLURALITY)
+        >>> profile
+        ProfileDiscrete({'abc': {0.2: 0.5}, 'acb': {0.4: 0.5}}, voting_rule='Plurality')
+        >>> print(profile)
+        <abc 0.2: 0.5, acb 0.4: 0.5> (Condorcet winner: a) (Plurality)
+    """
+    pass
+
+
+def test_anti_plurality():
+    """
+        >>> profile = ProfileDiscrete({('abc', 0.2): 1, ('acb', 0.4): 1}, voting_rule=ANTI_PLURALITY)
+        >>> profile
+        ProfileDiscrete({'abc': {0.2: 0.5}, 'acb': {0.4: 0.5}}, voting_rule='Anti-plurality')
+        >>> print(profile)
+        <abc 0.2: 0.5, acb 0.4: 0.5> (Condorcet winner: a) (Anti-plurality)
+    """
+    pass
--- tests/test_profile_histogram.py
+++ tests/test_profile_histogram.py
@@ -1,5 +1,5 @@
 import numpy as np
-from poisson_approval import ProfileHistogram, StrategyThreshold, EquilibriumStatus
+from poisson_approval import ProfileHistogram, StrategyThreshold, EquilibriumStatus, PLURALITY, ANTI_PLURALITY
 
 
 def test_normalization():
@@ -14,3 +14,31 @@ def test_not_equilibrium():
                                d_ranking_histogram = {'abc': [1], 'acb': [1]})
     strategy = StrategyThreshold(d_ranking_threshold={'abc': 0, 'acb': 0})
     assert profile.is_equilibrium(strategy) == EquilibriumStatus.NOT_EQUILIBRIUM
+
+
+def test_plurality():
+    """
+        >>> profile = ProfileHistogram(d_ranking_share={'abc': 1, 'acb': 1},
+        ...                            d_ranking_histogram = {'abc': [1, 1], 'acb': [1]},
+        ...                            voting_rule=PLURALITY)
+        >>> profile
+        ProfileHistogram({'abc': 0.5, 'acb': 0.5}, {'abc': array([0.5, 0.5]), 'acb': array([1])}, \
+voting_rule='Plurality')
+        >>> print(profile)
+        <abc: 0.5 [0.5 0.5], acb: 0.5 [1]> (Condorcet winner: a) (Plurality)
+    """
+    pass
+
+
+def test_anti_plurality():
+    """
+        >>> profile = ProfileHistogram(d_ranking_share={'abc': 1, 'acb': 1},
+        ...                            d_ranking_histogram = {'abc': [1, 1], 'acb': [1]},
+        ...                            voting_rule=ANTI_PLURALITY)
+        >>> profile
+        ProfileHistogram({'abc': 0.5, 'acb': 0.5}, {'abc': array([0.5, 0.5]), 'acb': array([1])}, \
+voting_rule='Anti-plurality')
+        >>> print(profile)
+        <abc: 0.5 [0.5 0.5], acb: 0.5 [1]> (Condorcet winner: a) (Anti-plurality)
+    """
+    pass
