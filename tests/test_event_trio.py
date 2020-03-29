f8fa77ab6ea2ec184bc9a839a5d578cff9d8898f
francois-durand <fradurandpub@gmail.com>
2020-03-29 13:31:45 +0200

    Update test_event_trio.py

--- tests/test_event_trio.py
+++ tests/test_event_trio.py
@@ -3,13 +3,15 @@ from poisson_approval import TauVector, EventTrio
 
 def test():
     tau = TauVector({
-        'a': 0.9999999868921308,
-        'ab': 7.182846803148341e-09,
-        'ac': 1.85196073297195e-09,
-        'b': 2.990584787561229e-09,
-        'bc': 1.082477022800632e-09,
+        'a': 1 - 11e-9,
+        'ab': 7e-09,
+        'ac': 1e-09,
+        'b': 2e-09,
+        'bc': 1e-09,
         'c': 0
     })
     assert tau.trio.phi_a <= 1
     assert tau.trio.phi_b <= 1
-    assert tau.trio.phi_c >= 1
+    assert tau.trio.psi_c >= 1
+    _ = tau.pivot_weak_ac
+    _ = tau.d_ranking_best_response['acb']
