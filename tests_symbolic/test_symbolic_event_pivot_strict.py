bccb8e9d1f5d2a3838a0be29b003421c70c3fccf
francois-durand <fradurandpub@gmail.com>
2020-03-29 15:37:22 +0200

    Update test_symbolic_event_pivot_strict.py

--- tests_symbolic/test_symbolic_event_pivot_strict.py
+++ tests_symbolic/test_symbolic_event_pivot_strict.py
@@ -20,8 +20,8 @@ phi_ac = 1, phi_bc = 1>
         <asymptotic = exp(- inf)>
         >>> tau = TauVector({'a': Fraction(1, 9), 'b': Fraction(1, 9),
         ...                  'ab': Fraction(1, 9), 'ac': Fraction(1, 3), 'bc': Fraction(1, 3)}, symbolic=True)
-        >>> EventPivotStrict(candidate_x='a', candidate_y='b', candidate_z='c', tau=tau)
-        <asymptotic = exp(- 0.0181103257505651*n + ? log(n) + ? + o(1)), phi_a = 1.17453803065111, \
-phi_b = 1.17453803065111, phi_ab = 1.37953958544578, phi_ac = 0.851398570249486, phi_bc = 0.851398570249486>
+        >>> EventPivotStrict(candidate_x='a', candidate_y='b', candidate_z='c', tau=tau)  # doctest: +ELLIPSIS
+        <asymptotic = exp(- 0.0181103...*n + ? log(n) + ? + o(1)), phi_a = 1.17453..., \
+phi_b = 1.17453..., phi_ab = 1.37953..., phi_ac = 0.851398..., phi_bc = 0.851398...>
     """
     pass
