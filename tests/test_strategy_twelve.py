73ff2160fb249d572ffaf8b760842e1f28c1e80e
francois-durand <fradurandpub@gmail.com>
2021-02-12 21:52:13 +0100

    Improve coverage

--- tests/test_strategy_twelve.py
+++ tests/test_strategy_twelve.py
@@ -33,24 +33,27 @@ def test_strategy_twelve_anti_plurality():
 
 
 def test_strategy_twelve_with_weak_order():
+    # APPROVAL
     with pytest.raises(ValueError):
         strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'b'},
-                                  d_weak_order_ballot = {'a~b>c': SPLIT}, voting_rule=APPROVAL)
+                                  d_weak_order_ballot={'a~b>c': SPLIT}, voting_rule=APPROVAL)
+    # PLURALITY
     with pytest.raises(ValueError):
         strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'b'},
-                                  d_weak_order_ballot = {'a>b~c': SPLIT}, voting_rule=PLURALITY)
+                                  d_weak_order_ballot={'a>b~c': SPLIT}, voting_rule=PLURALITY)
     with pytest.raises(ValueError):
         strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'b'},
-                                  d_weak_order_ballot = {'not a weak order': 'a'}, voting_rule=PLURALITY)
+                                  d_weak_order_ballot={'not a weak order': 'a'}, voting_rule=PLURALITY)
     with pytest.raises(ValueError):
         strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'b'},
-                                  d_weak_order_ballot = {'a~b>c': 'non-existing ballot'}, voting_rule=PLURALITY)
+                                  d_weak_order_ballot={'a~b>c': 'non-existing ballot'}, voting_rule=PLURALITY)
+    # ANTI_PLURALITY
     with pytest.raises(ValueError):
-        strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'b'},
-                                  d_weak_order_ballot = {'a~b>c': SPLIT}, voting_rule=ANTI_PLURALITY)
+        strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'ab'},
+                                  d_weak_order_ballot={'a~b>c': SPLIT}, voting_rule=ANTI_PLURALITY)
     with pytest.raises(ValueError):
-        strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'b'},
-                                  d_weak_order_ballot = {'not a weak order': 'a'}, voting_rule=ANTI_PLURALITY)
+        strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'ab'},
+                                  d_weak_order_ballot={'not a weak order': 'a'}, voting_rule=ANTI_PLURALITY)
     with pytest.raises(ValueError):
-        strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'b'},
-                                  d_weak_order_ballot = {'a~b>c': 'non-existing ballot'}, voting_rule=ANTI_PLURALITY)
+        strategy = StrategyTwelve({'abc': 'utility-dependent', 'bac': 'ab'},
+                                  d_weak_order_ballot={'a>b~c': 'non-existing ballot'}, voting_rule=ANTI_PLURALITY)
